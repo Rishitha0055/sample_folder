dd741c2c0c705a736b7ceca22d92b902542e709f
Rishitha0055 <rishitha.k@timingindia.com>
2022-07-20 12:19:17 +0530

    intial_commit

--- multiple.py
+++ multiple.py
@@ -8,7 +8,7 @@ def multiplyList(myList) :
     return result
      
 # Driver code
-list1 = [4, 5, 7]
-list2 = [3, 4, 5]
+list1 = [10, 5, 7]
+list2 = [3, 4, 8]
 print(multiplyList(list1))
 print(multiplyList(list2))
--- sample_code.py
+++ sample_code.py
@@ -1,6 +1,6 @@
 def add_number(n1,n2):
     sum = n1 + n2;
     return sum;
-num1 = 30
+num1 = 50
 num2 = 50
 print("The sum of two number is",add_number(num1,num2))
